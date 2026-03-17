f097b3fcc67dc212a7dd8f0e2325f07ad5cbad24
Zaid Malik <mzaidmalik25@gmail.com>
2026-03-17 23:19:09 +0530

    feat(logging): add morgan for HTTP request logging

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cookieParser from "cookie-parser"
 import cors from "cors"
+import morgan from "morgan"
 
 //Routes
 import UserRoutes from "./routes/user.routes.js"
@@ -22,6 +23,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 app.use(globalLimiter);
+app.use(morgan("dev"))
 
 app.get("/" , (req, res) => {
   res.send("HIII GYUS 😅")
